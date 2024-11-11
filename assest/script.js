1f4bc4c01c005f081d3f904f722672210047aa5b
hortdev <hort.webdev@gmail.com>
2024-11-11 16:36:48 +0700

    Updated

--- assest/script.js
+++ assest/script.js
@@ -12,6 +12,25 @@ const searchInput = document.querySelector('.search');
         searchBox.classList.remove('focused');
     });
 
+// go back to top button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 // JavaScript for staggered animation with Intersection Observer
 document.addEventListener("DOMContentLoaded", () => {
@@ -68,3 +87,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     packageCards.forEach(card => observer.observe(card));
 });
+
+// Send Mail Function
+function sendMail(){
+    let parms = {
+        email : document.getElementById("email").value,
+        phone : document.getElementById("phone").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_0gyzcta","template_yukthbi",parms).then(alert("Email Sent!"))
+}
